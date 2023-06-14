d10d1cd29c6f5155c6d5dcf2d298dd627e7e9e74
Ron Heft <ron@ronaldheft.com>
2023-06-14 19:08:28 +0000

    Support for Pepco (MD,DC)

---
+++ src/opower/utilities/pepco.py
@@ -0,0 +1,101 @@
+"""Potomac Electric Power Company (Pepco)."""
+
+import json
+import re
+
+import aiohttp
+from aiohttp.client_exceptions import ClientResponseError
+
+from .base import UtilityBase
+
+
+class Pepco(UtilityBase):
+    """Potomac Electric Power Company (Pepco)."""
+
+    @staticmethod
+    def name() -> str:
+        """Distinct recognizable name of the utility."""
+        return "Potomac Electric Power Company (Pepco)"
+
+    @staticmethod
+    def subdomain() -> str:
+        """Return the opower.com subdomain for this utility."""
+        return "pep"
+
+    @staticmethod
+    def timezone() -> str:
+        """Return the timezone."""
+        return "America/New_York"
+
+    @staticmethod
+    async def async_login(
+        session: aiohttp.ClientSession, username: str, password: str
+    ) -> None:
+        """Login to the utility website and authorize opower."""
+        async with session.get(
+            "https://secure.pepco.com/Pages/Login.aspx?/login"
+        ) as resp:
+            result = await resp.text()
+        # transId = "StateProperties=..."
+        # policy = "B2C_1A_SignIn"
+        # tenant = "/euazurebge.onmicrosoft.com/B2C_1A_SignIn"
+        # api = "CombinedSigninAndSignup"
+        settings = json.loads(re.search(r"var SETTINGS = ({.*});", result).group(1))
+
+        async with session.post(
+            "https://secure.exeloncorp.com"
+            + settings["hosts"]["tenant"]
+            + "/SelfAsserted",
+            params={
+                "tx": settings["transId"],
+                "p": settings["hosts"]["policy"],
+            },
+            data={
+                "request_type": "RESPONSE",
+                "signInName": username,
+                "password": password,
+            },
+            headers={
+                "X-CSRF-TOKEN": settings["csrf"],
+            },
+        ) as resp:
+            result = json.loads(await resp.text())
+
+        if result["status"] != "200":
+            raise ClientResponseError(
+                resp.request_info,
+                resp.history,
+                status=403,
+                message=result["message"],
+            )
+
+        async with session.get(
+            "https://secure.exeloncorp.com"
+            + settings["hosts"]["tenant"]
+            + "/api/"
+            + settings["api"]
+            + "/confirmed",
+            params={
+                "rememberMe": settings["config"]["enableRememberMe"],
+                "csrf_token": settings["csrf"],
+                "tx": settings["transId"],
+                "p": settings["hosts"]["policy"],
+                "diags": json.dumps(
+                    {
+                        "pageViewId": settings["pageViewId"],
+                        "pageId": settings["api"],
+                        "trace": [],
+                    }
+                ),
+            },
+        ) as resp:
+            result = await resp.text()
+
+        async with session.post(
+            "https://secure.pepco.com/api/Services/OpowerService.svc/GetOpowerToken",
+            json={},
+        ) as resp:
+            result = await resp.json()
+        access_token = result["access_token"]
+
+        session.headers.add("authorization", f"Bearer {access_token}")
