fb911667308078c434405bae695ae1f2c0644ec7
tronikos <tronikos@users.noreply.github.com>
2023-06-14 23:29:31 -0700

    Refactor code in BGE to Exelon base class

--- src/opower/utilities/bge.py
+++ src/opower/utilities/bge.py
@@ -1,15 +1,10 @@
 """Baltimore Gas and Electric (BGE)."""
 
-import json
-import re
-
-import aiohttp
-from aiohttp.client_exceptions import ClientResponseError
-
 from .base import UtilityBase
+from .exelon import Exelon
 
 
-class BGE(UtilityBase):
+class BGE(Exelon, UtilityBase):
     """Baltimore Gas and Electric (BGE)."""
 
     @staticmethod
@@ -23,77 +18,6 @@ class BGE(UtilityBase):
         return "bgec"
 
     @staticmethod
-    def timezone() -> str:
-        """Return the timezone."""
-        return "America/New_York"
-
-    @staticmethod
-    async def async_login(
-        session: aiohttp.ClientSession, username: str, password: str
-    ) -> None:
-        """Login to the utility website and authorize opower."""
-        async with session.get(
-            "https://secure.bge.com/Pages/Login.aspx?/login"
-        ) as resp:
-            result = await resp.text()
-        # transId = "StateProperties=..."
-        # policy = "B2C_1A_SignIn"
-        # tenant = "/euazurebge.onmicrosoft.com/B2C_1A_SignIn"
-        # api = "CombinedSigninAndSignup"
-        settings = json.loads(re.search(r"var SETTINGS = ({.*});", result).group(1))
-
-        async with session.post(
-            "https://secure2.bge.com" + settings["hosts"]["tenant"] + "/SelfAsserted",
-            params={
-                "tx": settings["transId"],
-                "p": settings["hosts"]["policy"],
-            },
-            data={
-                "request_type": "RESPONSE",
-                "signInName": username,
-                "password": password,
-            },
-            headers={
-                "X-CSRF-TOKEN": settings["csrf"],
-            },
-        ) as resp:
-            result = json.loads(await resp.text())
-
-        if result["status"] != "200":
-            raise ClientResponseError(
-                resp.request_info,
-                resp.history,
-                status=403,
-                message=result["message"],
-            )
-
-        async with session.get(
-            "https://secure2.bge.com"
-            + settings["hosts"]["tenant"]
-            + "/api/"
-            + settings["api"]
-            + "/confirmed",
-            params={
-                "rememberMe": settings["config"]["enableRememberMe"],
-                "csrf_token": settings["csrf"],
-                "tx": settings["transId"],
-                "p": settings["hosts"]["policy"],
-                "diags": json.dumps(
-                    {
-                        "pageViewId": settings["pageViewId"],
-                        "pageId": settings["api"],
-                        "trace": [],
-                    }
-                ),
-            },
-        ) as resp:
-            result = await resp.text()
-
-        async with session.post(
-            "https://secure.bge.com/api/Services/OpowerService.svc/GetOpowerToken",
-            json={},
-        ) as resp:
-            result = await resp.json()
-        access_token = result["access_token"]
-
-        session.headers.add("authorization", f"Bearer {access_token}")
+    def login_domain() -> str:
+        """Return the domain that hosts the login page."""
+        return "secure.bge.com"
---
+++ src/opower/utilities/exelon.py
@@ -0,0 +1,103 @@
+"""Base class for Exelon subsidiaries."""
+
+import json
+import re
+
+import aiohttp
+from aiohttp.client_exceptions import ClientResponseError
+
+
+class Exelon:
+    """Base class for Exelon subsidiaries."""
+
+    # Can find the opower.com subdomain using the GetConfiguration endpoint
+    # e.g. https://secure.bge.com/api/Services/MyAccountService.svc/GetConfiguration
+    # returns bgec.opower.com
+
+    @staticmethod
+    def timezone() -> str:
+        """Return the timezone."""
+        return "America/New_York"
+
+    @staticmethod
+    def login_domain() -> str:
+        """Return the domain that hosts the login page."""
+        raise NotImplementedError
+
+    @classmethod
+    async def async_login(
+        cls, session: aiohttp.ClientSession, username: str, password: str
+    ) -> None:
+        """Login to the utility website and authorize opower."""
+        async with session.get(
+            "https://" + cls.login_domain() + "/Pages/Login.aspx?/login"
+        ) as resp:
+            result = await resp.text()
+        # transId = "StateProperties=..."
+        # policy = "B2C_1A_SignIn"
+        # tenant = "/euazurebge.onmicrosoft.com/B2C_1A_SignIn"
+        # api = "CombinedSigninAndSignup"
+        settings = json.loads(re.search(r"var SETTINGS = ({.*});", result).group(1))
+        login_post_domain = resp.real_url.host
+
+        async with session.post(
+            "https://"
+            + login_post_domain
+            + settings["hosts"]["tenant"]
+            + "/SelfAsserted",
+            params={
+                "tx": settings["transId"],
+                "p": settings["hosts"]["policy"],
+            },
+            data={
+                "request_type": "RESPONSE",
+                "signInName": username,
+                "password": password,
+            },
+            headers={
+                "X-CSRF-TOKEN": settings["csrf"],
+            },
+        ) as resp:
+            result = json.loads(await resp.text())
+
+        if result["status"] != "200":
+            raise ClientResponseError(
+                resp.request_info,
+                resp.history,
+                status=403,
+                message=result["message"],
+            )
+
+        async with session.get(
+            "https://"
+            + login_post_domain
+            + settings["hosts"]["tenant"]
+            + "/api/"
+            + settings["api"]
+            + "/confirmed",
+            params={
+                "rememberMe": settings["config"]["enableRememberMe"],
+                "csrf_token": settings["csrf"],
+                "tx": settings["transId"],
+                "p": settings["hosts"]["policy"],
+                "diags": json.dumps(
+                    {
+                        "pageViewId": settings["pageViewId"],
+                        "pageId": settings["api"],
+                        "trace": [],
+                    }
+                ),
+            },
+        ) as resp:
+            result = await resp.text()
+
+        async with session.post(
+            "https://"
+            + cls.login_domain()
+            + "/api/Services/OpowerService.svc/GetOpowerToken",
+            json={},
+        ) as resp:
+            result = await resp.json()
+        access_token = result["access_token"]
+
+        session.headers.add("authorization", f"Bearer {access_token}")
