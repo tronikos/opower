a62053d5504d1e3f8f1331bc178eddd685852eb6
tronikos <tronikos@users.noreply.github.com>
2023-08-14 11:19:20 -0700

    Update opower.py

--- src/opower/opower.py
+++ src/opower/opower.py
@@ -254,9 +254,9 @@ class Opower:
                     if DEBUG_LOG_RESPONSE:
                         _LOGGER.debug("Fetched: %s", json.dumps(result, indent=2))
             except ClientResponseError as err:
-                # Some utilities don't provide forecast and return 404
+                # For some customers utilities don't provide forecast and return 404
                 if err.status == 404:
-                    return []
+                    continue
             for forecast in result["accountForecasts"]:
                 forecasts.append(
                     Forecast(
