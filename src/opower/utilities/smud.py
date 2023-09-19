45435dd22352ca0322267977d7053e7cab1ee4a6
andylittle <rocklin662@gmail.com>
2023-09-19 06:57:56 -0700

    proof of concept getsokta token this works as far as getting the token

--- src/opower/utilities/smud.py
+++ src/opower/utilities/smud.py
@@ -23,7 +23,7 @@ class SMUDLoginParser(HTMLParser):
         """Try to extract the verification token from the login input."""
         if tag == "input" and ("name", "__RequestVerificationToken") in attrs:
             _, token = next(filter(lambda attr: attr[0] == "value", attrs))
-            print(token)
+            print("self verify token:", token)
             self.verification_token = token
 
 
@@ -119,25 +119,49 @@ class SMUD(UtilityBase):
         )
 
         usage_parser = SMUDUsageParser()
+        #session: aiohttp.ClientSession,
         async with session.get(
             #"https://www.pse.com/api/AccountSelector/GetContractAccountJson",
             "https://myaccount.smud.org/manage/opowerresidential/energyusage",
             allow_redirects=True,
             headers={"User-Agent": USER_AGENT},
             raise_for_status=True,
-        ) as resp:
+#        ) as resp:
             #if len(await resp.text()) == 0:
             #    raise InvalidAuth("Login failed")
-            usage_parser.feed(await resp.history[1])
+            #rhistory = resp.history
+####            print(type(resp.history))
+ ###           print('history: ', resp.history[1])
+ ###           print()
+            #usage_parser.feed(rhistory)
             #print('text: ', resp.text)
             #print('url: ', resp.url)
             #print('status: ', resp.status)
-            print('history: ', resp.history[1])  ### The token is in here
+
+          ) as resp:
+            #print('resp: ', resp)
+            #print('TYPE RESP:', type(resp))
+            #print('TYPE RESP.HIST:', type(resp.history))
+
+            rhistory = resp.history
+            rhistory = str(rhistory[1])
+            #print('TYPE RHISTORY', type(rhistory))
+            print('history: ', rhistory)
+            regexp = re.compile(r'https:\/\/smud\.okta\.com\/login\/sessionCookieRedirect\?token=(?P<token>.+)&redirect.*')
+            result = regexp.search(rhistory)
+            print('-----------------' )
+            if result and result.group("token"):
+                #self.opower_access_token = result.group("token")
+                print(result.group("token"))
+                print()
+            exit()
+            usage_parser.feed(rhistory)
 
             assert (
                 usage_parser.opower_access_token
             ), "Failed to parse OPower bearer token"
-        print(usage_parser.opower_access_token)
+
+        print("Access Token:", usage_parser.opower_access_token)
         return usage_parser.opower_access_token
 
        # async with session.get(
