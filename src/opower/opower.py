d375e3e41fe24409991c15fd2ff63231f54c5ac8
tronikos <tronikos@users.noreply.github.com>
2023-11-02 22:57:05 -0700

    Do not skip empty forecasts in beginning of bill

--- src/opower/opower.py
+++ src/opower/opower.py
@@ -251,7 +251,7 @@ class Opower:
             if all(
                 x in result["totalMetadata"]
                 for x in ["NO_FORECASTED_COST", "NO_FORECASTED_USAGE"]
-            ):
+            ) and "DATA_COVERAGE_QUALITY_CHECK_FAILED" not in result["totalMetadata"]:
                 _LOGGER.debug(
                     "Ignoring combined-forecast since there is no usage or cost. metadata: %s",
                     result["totalMetadata"],
