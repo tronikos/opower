a3e2d4aa5698ca4b5814c9e4c52e8e4f1311ca6b
tronikos <tronikos@users.noreply.github.com>
2023-12-14 03:10:50 -0800

    Extract helper SAML auth for reuse

---
+++ src/opower/utilities/helpers.py
@@ -0,0 +1,51 @@
+"""Helper functions."""
+
+import re
+
+import aiohttp
+
+from ..const import USER_AGENT
+
+
+def get_form_action_url_and_hidden_inputs(html: str) -> tuple[str, dict[str, str]]:
+    """Return the URL and hidden inputs from the single form in a page."""
+    match = re.search(r'action="([^"]*)"', html)
+    if not match:
+        return "", {}
+    action_url = match.group(1)
+    inputs = {}
+    for match in re.finditer(
+        r'input\s*type="hidden"\s*name="([^"]*)"\s*value="([^"]*)"', html
+    ):
+        inputs[match.group(1)] = match.group(2)
+    return action_url, inputs
+
+
+async def async_auth_saml(session: aiohttp.ClientSession, url: str) -> None:
+    """Authenticate with Opower using SAML."""
+    # Fetch the URL on the utility website to get RelayState and SAMLResponse.
+    async with session.get(url) as resp:
+        result = await resp.text()
+    action_url, hidden_inputs = get_form_action_url_and_hidden_inputs(result)
+    assert action_url.endswith(".opower.com/sp/ACS.saml2")
+    assert set(hidden_inputs.keys()) == {"RelayState", "SAMLResponse"}
+
+    # Pass them to opower.com/sp/ACS.saml2 to get opentoken.
+    async with session.post(
+        action_url,
+        data=hidden_inputs,
+        headers={"User-Agent": USER_AGENT},
+        raise_for_status=True,
+    ) as resp:
+        result = await resp.text()
+    action_url, hidden_inputs = get_form_action_url_and_hidden_inputs(result)
+    assert set(hidden_inputs.keys()) == {"opentoken"}
+
+    # Pass it back to the utility website.
+    async with session.post(
+        action_url,
+        data=hidden_inputs,
+        headers={"User-Agent": USER_AGENT},
+        raise_for_status=True,
+    ) as resp:
+        pass
--- src/opower/utilities/pge.py
+++ src/opower/utilities/pge.py
@@ -9,6 +9,7 @@ import aiohttp
 from ..const import USER_AGENT
 from ..exceptions import InvalidAuth
 from .base import UtilityBase
+from .helpers import async_auth_saml
 
 
 def _get_form_action_url_and_hidden_inputs(html: str) -> tuple[str, dict[str, str]]:
@@ -116,29 +117,4 @@ class PGE(UtilityBase):
             "https://pge.opower.com/ei/app/r/energy-usage-details"
         )
 
-        # Fetch the URL on the utility website to get RelayState and SAMLResponse.
-        async with session.get(url) as resp:
-            result = await resp.text()
-        action_url, hidden_inputs = _get_form_action_url_and_hidden_inputs(result)
-        assert action_url == "https://sso2.opower.com/sp/ACS.saml2"
-        assert set(hidden_inputs.keys()) == {"RelayState", "SAMLResponse"}
-
-        # Pass them to https://sso2.opower.com/sp/ACS.saml2 to get opentoken.
-        async with session.post(
-            action_url,
-            data=hidden_inputs,
-            headers={"User-Agent": USER_AGENT},
-            raise_for_status=True,
-        ) as resp:
-            result = await resp.text()
-        action_url, hidden_inputs = _get_form_action_url_and_hidden_inputs(result)
-        assert set(hidden_inputs.keys()) == {"opentoken"}
-
-        # Pass it back to the utility website.
-        async with session.post(
-            action_url,
-            data=hidden_inputs,
-            headers={"User-Agent": USER_AGENT},
-            raise_for_status=True,
-        ) as resp:
-            pass
+        await async_auth_saml(session, url)
