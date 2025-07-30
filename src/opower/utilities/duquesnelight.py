00f1652fff4c4068427742d1e906b05cf05e2368
David Sevilla Martin <6401250+dsevillamartin@users.noreply.github.com>
2025-07-29 22:03:22 -0400

    Update Duquesne Light login flow

--- src/opower/utilities/duquesnelight.py
+++ src/opower/utilities/duquesnelight.py
@@ -65,7 +65,8 @@ class DuquesneLight(UtilityBase):
     ) -> str:
         """Login to the utility website."""
         # Double-logins are somewhat broken if cookies stay around.
-        session.cookie_jar.clear(lambda cookie: cookie["domain"] == "www.duquesnelight.com")
+        # Some use auth.duquesnelight.com and others use duquesnelight.com (session token)
+        session.cookie_jar.clear(lambda cookie: "duquesnelight.com" in cookie["domain"])
         # DQE uses Incapsula and merely passing the User-Agent is not enough.
         headers = {
             "User-Agent": USER_AGENT,
@@ -82,27 +83,20 @@ class DuquesneLight(UtilityBase):
         }
 
         async with session.post(
-            "https://www.duquesnelight.com/login/login",
+            "https://auth.duquesnelight.com/oauth/authorize/login",
             data={
-                "Phone": "",
-                "Email": "",
-                "IsLoginOff": "false",
-                "RedFlagPassword": "",
-                "RememberUsername": "false",
-                "Username": username,
-                "Password": password,
-                "RedirectUrl": "/my-account/account-summary",
-                "SuppressPleaseLoginMessage": "true",
-                "LoginTurnedOffMessage": "",
-                "RedirectPath": "",
-                "PersonId": "",
+                "grant_type": "password",
+                "remember_username": True,
+                "username": username,
+                "password": password,
             },
             headers=headers,
-            raise_for_status=True,
+            raise_for_status=False,
         ) as resp:
-            # Check for failed login - DQE returns status 200 with a json body that can be parsed.
-            if "invalid" in await resp.text():
-                raise InvalidAuth("Login failed")
+            if resp.status == 400:
+                raise InvalidAuth(await resp.text())
+            elif resp.status != 200:
+                resp.raise_for_status()
 
         usage_parser = DQEUsageParser()
 
